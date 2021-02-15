88a5c3cce3cfb0a1fc72151618ae33552337e043
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2021-02-15 13:44:26 -0300

    Tutorial dapp university

--- truffle-config.js
+++ truffle-config.js
@@ -3,9 +3,14 @@ require('babel-polyfill');
 
 module.exports = {
   networks: {
-    development: {
+    ganache: {
       host: "127.0.0.1",
       port: 7545,
+      network_id: "*"
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
       network_id: "*" // Match any network id
     },
   },
